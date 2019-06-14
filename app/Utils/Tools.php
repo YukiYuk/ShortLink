83f9b96415f8f4fcf9d4821b896e437350b44094
WooMai <allentian666@gmail.com>
2019-06-14 21:50:22 +0800

    fix

--- app/Utils/Tools.php
+++ app/Utils/Tools.php
@@ -12,7 +12,7 @@ class Tools
      */
     public static function isValidURL($url) {
         if (parse_url($url, PHP_URL_HOST) == Config::get('host')) return false;
-        if (filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED)) {
+        if (filter_var($url, FILTER_VALIDATE_URL)) {
             $proto = explode('://', $url)[0];
             if (
                 $proto == 'http' ||
